8105a4b1bdb97ef73450171f261bf68114b07979
Karl Dreher <karldreher@outlook.com>
2020-03-03 22:46:09 -0700

    Cleanup for readability.

--- smile.js
+++ smile.js
@@ -1,20 +1,22 @@
 
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
-	url = details.url
-	if (url.match("pldnSite=1")){
-		//This ID comes from Amazon and appears to identify if the page is already on the "prompt page" for amazon smile.  
-		//if this is not supported in the future the extension will probably supply its own.  
-	}
-	//All clear?  redirect the page.  
-	else{
-		console.log(url)
-		newurl = url.replace("www.amazon.","smile.amazon.")
-		console.log(newurl)
-		return {
-			redirectUrl: newurl
-		};
-	}
-}, {
-    urls : ["https://www.amazon.com/*","https://www.amazon.de/*"],
-    types: ["main_frame","sub_frame"]
-}, ["blocking"]);
+chrome.webRequest.onBeforeRequest.addListener(
+	function(details) {
+		url = details.url
+		if (url.match("pldnSite=1")){
+			//This ID comes from Amazon and appears to identify if the page is already on the "prompt page" for amazon smile.  
+			//if this is not supported in the future the extension will probably supply its own.  
+		}
+		
+		else{
+			//All clear?  redirect the page.  
+			console.log(url)
+			newurl = url.replace("www.amazon.","smile.amazon.")
+			console.log(newurl)
+			return {redirectUrl: newurl};
+		}
+	}, 
+
+	{urls : ["https://www.amazon.com/*","https://www.amazon.de/*"],
+	types: ["main_frame","sub_frame"]}, 
+	["blocking"]
+);
