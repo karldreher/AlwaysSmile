dab97153b75c712d8937e59c7e20f88c1c3b783c
Karl Dreher <karldreher@outlook.com>
2020-10-03 13:47:13 -0700

    Better file naming structure

--- build/build-chrome.sh
+++ build/build-chrome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo $browser
+
 cd ../chrome
 cp ../icons/*.png .
 cp ../*.js .
-zip $browser-extension-$version *
+zip $filename *
--- build/build-firefox.sh
+++ build/build-firefox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo $browser
+
 cd ../firefox
 cp ../icons/*.png .
 cp ../*.js .
-zip $browser-extension-$version *
+zip $filename *
